24a951258c87cfdd8eee72a58024063fdc5309db
Stas <113462863+shamansk1y@users.noreply.github.com>
2023-09-23 21:11:15 -0400

    Update parsers.py

--- scraping/parsers.py
+++ scraping/parsers.py
@@ -28,7 +28,7 @@ def initialize_driver(url):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
 
