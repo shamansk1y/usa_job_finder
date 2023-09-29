a43a322fb5be2a2fde9cb9f7dfa9127c89d22e7b
Stas <113462863+shamansk1y@users.noreply.github.com>
2023-09-29 09:22:18 -0400

    Update parsers.py

--- scraping/parsers.py
+++ scraping/parsers.py
@@ -3,7 +3,6 @@ from fake_useragent import UserAgent
 from bs4 import BeautifulSoup as BS
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.service import Service as ChromeService
 import time
 import requests
 import os
@@ -13,6 +12,10 @@ __all__ = ('indeed', 'linkedin', 'zip_recruiter', 'dice', 'ladders', 'flexjobs' 
 ua = UserAgent()
 headers = {'User-Agent': ua.random}
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
 
 def initialize_driver(url):
     # local config
@@ -25,12 +28,7 @@ def initialize_driver(url):
 
 
     # deploy config
-    chrome_service = ChromeService(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     
     driver.get(url)
