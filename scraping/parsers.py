616fb52200c5c33d23e70371afd5ffaaf985e6ec
Stas <113462863+shamansk1y@users.noreply.github.com>
2023-09-29 15:25:53 -0400

    Update parsers.py

--- scraping/parsers.py
+++ scraping/parsers.py
@@ -13,7 +13,7 @@ ua = UserAgent()
 headers = {'User-Agent': ua.random}
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
 
